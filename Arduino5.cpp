7d35c8794ad7e4a42c4165bd4ee5205ca8f5993d
ChiranjithChkz <chiranjithchakma09@gmail.com>
2025-11-01 21:11:28 +0600

    Simple Operation

---
+++ Arduino5.cpp
@@ -0,0 +1,16 @@
+/***
+ * // A Simple Program
+
+void setup()
+{
+  Serial.begin(9600); //initialization
+  int a=3, b=9;
+  int sum= a+b;
+  Serial.println(sum); //print
+}
+
+void loop()
+{
+  
+}
+ */
