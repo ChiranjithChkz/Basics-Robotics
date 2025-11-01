e94928d7cc475698f5fa550804cf63f013f92077
ChiranjithChkz <chiranjithchakma09@gmail.com>
2025-11-01 21:03:51 +0600

    Preface of basic operator

---
+++ Arduino4.cpp
@@ -0,0 +1,16 @@
+/**
+ * //Operator: Arithmetic, Comparison, Boolean.
+// Arithmetic: +, -,*, /.
+//Comparison: >, <, >= , <=, ==, !=.
+//Boolean: &&(AND), ||(OR). 
+
+void setup()
+{
+ 
+}
+
+void loop()
+{
+  
+}
+ */
