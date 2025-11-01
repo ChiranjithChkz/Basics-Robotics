0c6f235979faffa2dc42eb8872bc244cb1ff40a1
ChiranjithChkz <chiranjithchakma09@gmail.com>
2025-11-01 20:51:05 +0600

    Variable assign and print

---
+++ arduino3.cpp
@@ -0,0 +1,27 @@
+/** 
+ * 
+ * //Variable Types: 
+//1. Integer : 1,2 ,3 ,4 ,5 200.
+//2. Float: 2.3 , 5.6, 7.5.
+//3. Char : 'a', 'f','d','e','r','w'.
+//4. String: "red", "ball", "car","I love robotics". 
+
+
+
+void setup()
+{
+  Serial.begin(9600);
+// Variable Declaration: data_type variable_name;
+   int robot=2;
+   float car;
+  car = 3.232;
+   char letter;
+   String word;
+  
+  Serial.println(car);
+}
+
+void loop()
+{
+  
+}*/ 
