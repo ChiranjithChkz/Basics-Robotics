8e6e2586c215e5d23d8b44b2a75f88c7dcff529e
ChiranjithChkz <chiranjithchakma09@gmail.com>
2025-11-02 11:41:48 +0600

    increment and decrement operator

---
+++ incre and decre operator.cpp
@@ -0,0 +1,21 @@
+/**
+ * 
+ * // increment and decrement Operator
+
+
+void setup()
+{
+  Serial.begin(9600); 
+  int i;
+  i =5;
+  Serial.println(i);
+  i++;
+  Serial.println(i);
+  Serial.println(i);
+}
+
+void loop()
+{
+  
+}
+ */
