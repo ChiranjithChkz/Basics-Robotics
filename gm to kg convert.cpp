350baf9ea81ea304defe8b9a471fd74a12bb3b7c
ChiranjithChkz <chiranjithchakma09@gmail.com>
2025-11-02 11:30:36 +0600

    gm to kg convert

---
+++ gm to kg convert.cpp
@@ -0,0 +1,18 @@
+/**
+ * // gm to kg converter Program
+
+
+void setup()
+{
+  Serial.begin(9600); 
+  int gm = 1500;
+  float kg;
+  kg = (float)gm / 1000;
+  Serial.println(kg);
+}
+
+void loop()
+{
+  
+}
+ */
