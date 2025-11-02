3ba1d06f6ffa93b21616da7116be654e3310c0be
ChiranjithChkz <chiranjithchakma09@gmail.com>
2025-11-02 11:25:44 +0600

    converter

---
+++ Arduino6.cpp
@@ -0,0 +1,26 @@
+/**
+ * 
+ * // km tom converter Program
+
+
+void setup()
+{
+  Serial.begin(9600);
+  int km=10;
+  int m;
+  
+  m = km * 1000;
+  
+  //fit to inch converter Program
+  
+  int fit = 10;
+  int inch;
+  inch = 12 * fit;
+  Serial.println(inch);
+}
+
+void loop()
+{
+  
+}
+ */
