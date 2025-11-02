27ed28a3b461ec02ad480a624616587e0d93d409
ChiranjithChkz <chiranjithchakma09@gmail.com>
2025-11-02 12:06:08 +0600

    IF-ELSE condition

---
+++ IF-ELSE condition.cpp
@@ -0,0 +1,27 @@
+/**
+ * //  Take an Input from Serial Monitor
+  //  IF-ELSE  Conditional Statement
+
+void setup()
+{
+  Serial.begin(9600);
+
+}
+
+void loop()
+{
+   if(Serial.available()> 0)
+   {
+          char data = Serial.read();
+         if(data == 'r' || data == 'R')
+             {
+                 Serial.println("Today is Rainy day");
+             }
+         else{
+                Serial.println("Today is Sunny day");
+             }
+                 delay(1000);
+   }
+
+}
+ */
