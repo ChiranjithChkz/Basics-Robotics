2f8218f3460102ad416ad8cf3511db6a6a9be404
ChiranjithChkz <chiranjithchakma09@gmail.com>
2025-11-03 18:38:20 +0600

    for-loop

---
+++ for-loop.cpp
@@ -0,0 +1,19 @@
+/**
+ * //  Take an Input from Serial Monitor
+   // for-Loop
+
+void setup()
+{
+  
+   Serial.begin(9600);
+    for(int i=1; i<=100; i++)
+    {
+      Serial.println(i); Serial.println("Hello Robo-Man");
+    }
+}
+
+void loop()
+{
+   
+}
+ */
