1a29a71f0814dacd2cb2aacdac1b91bcb211f6a4
ChiranjithChkz <chiranjithchakma09@gmail.com>
2025-11-03 19:11:16 +0600

    control- loop

---
+++ Control-while-loop.cpp
@@ -0,0 +1,27 @@
+/**
+ * //  Take an Input from Serial Monitor
+    //Infinitive For loop
+
+void setup()
+{
+  Serial.begin(9600);
+    
+  
+  while(1)
+  {
+   if(Serial.read() == 's')
+   {
+       while(1)
+             {
+                 Serial.println("Hello");
+                 if(Serial.read() == 'b')
+                      { // get input from users
+                           break;
+                      }
+               }
+          Serial.println("Loop Broken");
+     }
+   
+  }
+}
+ */
