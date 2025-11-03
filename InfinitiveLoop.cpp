74f9757916af84212ef4201ee98a479e92beedf7
ChiranjithChkz <chiranjithchakma09@gmail.com>
2025-11-03 18:57:32 +0600

    Infinitive loop

---
+++ InfinitiveLoop.cpp
@@ -0,0 +1,19 @@
+/**
+ * //  Take an Input from Serial Monitor
+ //Infinitive For loop
+
+void setup()
+{
+  Serial.begin(9600);
+   for( ;  ;)
+   {
+     Serial.println("Infinitive Loop");
+                    }
+}
+
+
+void loop()
+{
+
+}
+ */
