2424710252f57ae0a5d2906cbe80d0f14785cfb7
ChiranjithChkz <chiranjithchakma09@gmail.com>
2025-11-05 13:24:45 +0600

    light on by code

---
+++ Arduino/light on.cpp
@@ -0,0 +1,21 @@
+/**
+ * // C++ code
+// LED on-off
+void setup()
+{
+  
+  // target pin
+  pinMode(7,OUTPUT);
+  
+}
+
+void loop()
+{
+  //TO TURN ON light
+  digitalWrite(7,HIGH);
+  delay(1000);
+  //TO TURN OFF LIGHT
+  digitalWrite(7,LOW);
+  delay(1000);
+}
+ */
