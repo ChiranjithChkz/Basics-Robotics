2169d728c27497b5571e8b003c3aba21806a0b81
ChiranjithChkz <chiranjithchakma09@gmail.com>
2025-11-08 20:20:06 +0600

    Automatic light on and off

---
+++ Projects/AutomaticOnOff.cpp
@@ -0,0 +1,30 @@
+/**
+ * // C++ code
+// first Project: Automatic light on-off lamp post
+// On when night, Off when day
+
+int light = 9;  // define
+void setup()
+{
+   Serial.begin(9600);
+   pinMode(light, OUTPUT); // select mode
+}
+
+void loop()
+{
+  
+  int resistance = analogRead(A0); //define
+  Serial.println(resistance);
+  
+  
+  //main condition
+  if(resistance <400)  
+  {
+    digitalWrite(light, HIGH);
+    delay(1000);
+  }
+  else {
+    digitalWrite(light, LOW);
+  }
+}
+ */
