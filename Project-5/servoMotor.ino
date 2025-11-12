4adbe0abf40ea731608931bff5e02a2f741578b8
ChiranjithChkz <chiranjithchakma09@gmail.com>
2025-11-12 09:56:16 +0600

    ServoMotor Use

---
+++ Project-5/servoMotor.ino
@@ -0,0 +1,30 @@
+// C++ code
+// flex sensor use
+// use of map function
+// servo motor uses
+
+#include<Servo.h>
+
+int flex_sensor = A0;
+int servo_pin = 7;
+
+Servo myServo;
+void setup()
+{
+   Serial.begin(9600);
+  myServo.attach(servo_pin);
+}
+
+void loop()
+{
+  int angle_value = angle();
+  myServo.write(angle_value);
+  Serial.println(angle_value);
+}
+
+int angle()
+{
+ int sensor_value = analogRead(A0);
+ int angle=  map(sensor_value, 682, 275,0,180);
+  return angle;
+}
--- Projects-2/MoistureCheck.ino
+++ Projects-2/MoistureCheck.ino
@@ -1,56 +1,56 @@
-// // C++ code
-//  // Check the soil moisture 
+// C++ code
+ // Check the soil moisture 
 
 
 
 
-// int sensor = A0;
-// int red = 7, blue=6, green=5;
-// void setup()
-// {
-//    Serial.begin(9600);
-//     pinMode(red, OUTPUT);
-//     pinMode(blue, OUTPUT);
-//     pinMode(green, OUTPUT);
+int sensor = A0;
+int red = 7, blue=6, green=5;
+void setup()
+{
+   Serial.begin(9600);
+    pinMode(red, OUTPUT);
+    pinMode(blue, OUTPUT);
+    pinMode(green, OUTPUT);
   
-// }
+}
 
-// void loop()
-// {
-//   int moisture = analogRead(sensor);
-//   Serial.println(moisture);
-//   Green();
+void loop()
+{
+  int moisture = analogRead(sensor);
+  Serial.println(moisture);
+  Green();
   
-//   if(moisture >= 0 && moisture <= 300)
-//   {
-//     Red();
-//   }
-//   if(moisture  >= 300 && moisture <= 600)
-//   {
-//     Yellow();
-//   }
-//   if(moisture > 600) {
-//     Green();
-//   }
-// }
+  if(moisture >= 0 && moisture <= 300)
+  {
+    Red();
+  }
+  if(moisture  >= 300 && moisture <= 600)
+  {
+    Yellow();
+  }
+  if(moisture > 600) {
+    Green();
+  }
+}
 
-// void Red(){
-//     digitalWrite(red, HIGH);
-//     digitalWrite(blue, LOW);
-//     digitalWrite(green, LOW);
+void Red(){
+    digitalWrite(red, HIGH);
+    digitalWrite(blue, LOW);
+    digitalWrite(green, LOW);
   
-// }
+}
 
-// void Yellow(){
-//   digitalWrite(red, HIGH);
-//   digitalWrite(green, HIGH);
-//   digitalWrite(blue, LOW);
-// }
+void Yellow(){
+  digitalWrite(red, HIGH);
+  digitalWrite(green, HIGH);
+  digitalWrite(blue, LOW);
+}
 
-// void Green(){
-//   digitalWrite(red, LOW);
-//   digitalWrite(green, HIGH);
-//   digitalWrite(blue, LOW);
-// }
+void Green(){
+  digitalWrite(red, LOW);
+  digitalWrite(green, HIGH);
+  digitalWrite(blue, LOW);
+}
 
 
